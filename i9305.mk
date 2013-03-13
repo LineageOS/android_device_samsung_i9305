ea04354af15e3670ebe1f2e5052f29d6e9733378
codeworkx <codeworkx@cyanogenmod.org>
2013-03-13 16:59:04 +0000

    i9305: move camera firmwares to common
    Change-Id: Idec417708b58e189f626f55c508f49dbb8c0e465

--- i9305.mk
+++ i9305.mk
@@ -41,10 +41,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
 
-# Camera FW
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/80cfw:system/etc/init.d/80cfw
-
 # Product specific Packages
 PRODUCT_PACKAGES += \
     GalaxyS3Settings

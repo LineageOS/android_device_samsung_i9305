f952216a36756579774fec918782ae56a5e85b8d
mark <mcampbellsmith@gmail.com>
2013-08-12 23:06:42 +1000

    i9305: enable low power bluetooth (BLE) permissions
    Change-Id: Iad5a6a35dd285940846b7f4f76fdbb1aefd6f050

--- i9305.mk
+++ i9305.mk
@@ -82,7 +82,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Include common makefile
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)

0920eb6428b5779e89b3f66b049ddbb5f9c9215d
RGIB <gibellini.roberto@gmail.com>
2016-11-20 14:26:52 +0100

    i9305 : build the correct sensors driver
    from stock sensors.smdk4412.so :
    
    LSM330DLC 3-axis Accelerometer
    AK8975C 3-axis Magnetic field sensor
    AK8975C Magnetic field Sensor UnCalibrated
    CM36651 Light sensor
    CM36651 Proximity sensor
    LSM330DLC Gyroscope sensor
    LPS331AP Pressure Sensor
    
    Change-Id: I24cdb73bbf91ccbf03be78e159df21cc780bc1e5

--- i9305.mk
+++ i9305.mk
@@ -53,6 +53,10 @@ PRODUCT_PACKAGES += \
     libsecril-client \
     libsecril-client-sap
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.smdk4x12
+
 PRODUCT_COPY_FILES += \
     packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \

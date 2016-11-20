e9458cfb18cc8aec0abe4f2c2bb4a35a5d659fdd
RGIB <gibellini.roberto@gmail.com>
2016-11-20 13:34:23 +0100

    i9305 : build libsecril-client libs
    Change-Id: Ic34b10d5a6e163432ad14b3bb308f872e061dce5

--- i9305.mk
+++ i9305.mk
@@ -48,6 +48,11 @@ PRODUCT_PACKAGES += \
     Nfc \
     Tag
 
+# RIL
+PRODUCT_PACKAGES += \
+    libsecril-client \
+    libsecril-client-sap
+
 PRODUCT_COPY_FILES += \
     packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \

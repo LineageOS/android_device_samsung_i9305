cc9711b48e8c5697c8571c71484b6ddfbbd799f6
Roberto Gibellini <gibellini.roberto@gmail.com>
2017-01-05 11:00:03 +0100

    i9305 : inline with RIL.java
    Change-Id: I5bd8cb0072afb9f04ec3a455e6024d68b250bd21

--- ril/telephony/java/com/android/internal/telephony/smdk4x12QComRIL.java
+++ ril/telephony/java/com/android/internal/telephony/smdk4x12QComRIL.java
@@ -48,6 +48,7 @@ import java.util.Collections;
 
 import com.android.internal.telephony.uicc.IccCardApplicationStatus;
 import com.android.internal.telephony.uicc.IccCardStatus;
+import com.android.internal.telephony.uicc.IccUtils;
 
 /**
  * Qualcomm RIL for the Samsung family.
@@ -301,7 +302,7 @@ public class smdk4x12QComRIL extends RIL implements CommandsInterface {
 
     @Override
     protected void
-    processUnsolicited (Parcel p) {
+    processUnsolicited (Parcel p, int type) {
         Object ret;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
         int response = p.readInt();
@@ -351,7 +352,7 @@ public class smdk4x12QComRIL extends RIL implements CommandsInterface {
                 p.setDataPosition(dataPosition);
 
                 // Forward responses that we are not overriding to the super class
-                super.processUnsolicited(p);
+                super.processUnsolicited(p, type);
                 return;
         }
 
@@ -372,7 +373,7 @@ public class smdk4x12QComRIL extends RIL implements CommandsInterface {
 
     @Override
     protected RILRequest
-    processSolicited (Parcel p) {
+    processSolicited (Parcel p, int type) {
         int serial, error;
         boolean found = false;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
@@ -406,7 +407,7 @@ public class smdk4x12QComRIL extends RIL implements CommandsInterface {
             /* Nothing we care about, go up */
             p.setDataPosition(dataPosition);
             // Forward responses that we are not overriding to the super class
-            return super.processSolicited(p);
+            return super.processSolicited(p, type);
         }
         rr = findAndRemoveRequestFromList(serial);
         if (rr == null) {
